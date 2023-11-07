f5dcb2d308c48d9fc705b5611b5a74ee3cc736a7
Lalit Jakhar <lalitjakhar@Rajeshs-iMac.local>
2023-11-07 14:19:17 +0530

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
-    <h1>hello</h1>
+    <h1>helldsafddsafrdsgfdgfrtghtrghregregreefedewreerewo</h1>
     </>
   )
 }
