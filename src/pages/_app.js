4024c374712ddb6ae545983111cd39888c88d334
Lalit Jakhar <lalitjakhar@Rajeshs-iMac.local>
2023-11-07 15:13:20 +0530

    initial commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,27 @@
-import '@/styles/globals.css'
+import Community from './Components/Community/Community';
+import Delight from './Components/Delight/Delight';
+import Experience from './Components/Experience/Experience';
+import Feedback from './Components/Feedback/Feedback';
+import Footer from './Components/Footer/Footer';
+import Hygienic from './Components/Hygienic/Hygienic';
+import Navbar from './Components/Navbar/Navbar';
+import Story from './Components/Story/Story';
+import Sustainable from './Components/Sustainable/Sustainable';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App() {
+  return (
+    <>
+    <Navbar />
+    <Experience />
+    <Story />
+    <Delight />
+    <Sustainable />
+    <Feedback />
+    <Hygienic />
+    <Community />
+    <Footer />
+    </>
+  );
 }
+
+export default App;
